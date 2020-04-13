1e4882d953b2ecef41a397f75ca4e98693a82d72
jeehong.lee@linecorp.com <jeehong.lee@linecorp.com>
2020-04-13 17:32:51 +0900

    feat: guessing game - loop

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,18 @@ fn main() {
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to readline as String");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
-        println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
